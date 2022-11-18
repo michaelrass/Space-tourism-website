c1b2dfc64a91c0a1010a255ec5ec7f618688c4ef
your_username <michaelras_@live.com>
2022-11-18 14:15:44 +0100

    header mobile burger menu and main-home mobile finished

--- js/modules/Header.js
+++ js/modules/Header.js
@@ -1,14 +1,20 @@
 export default function Header() {
-
+	let burgerMenuVisible = false;
+	
 	const buttonHome = document.querySelector('.navigation__button-home');
 	const buttonDestination = document.querySelector('.navigation__button-destination');
 	const buttonCrew = document.querySelector('.navigation__button-crew');
 	const buttonTechnology = document.querySelector('.navigation__button-technology');
+	const buttonBurgerMenu = document.querySelector('.header__burger-icon-container');
+	const asideMenu = document.querySelector('.header__aside--hidden');
+	const buttonCloseAsideMenu = document.querySelector('.button-close-aside');
 
 	buttonHome.addEventListener('click', handleButtonHomeClick);
 	buttonDestination.addEventListener('click', handleButtonDestinationClick);
 	buttonCrew.addEventListener('click', handleButtonCrewClick);
 	buttonTechnology.addEventListener('click', handleButtonTechnologyClick);
+	buttonBurgerMenu.addEventListener('click', handleButtonBurgerMenuClick);
+	buttonCloseAsideMenu.addEventListener('click', handleButtonCloseAsideMenuClick);
 
 	function handleButtonHomeClick() {
 		goToHomeHtml();
@@ -26,6 +32,16 @@ export default function Header() {
 		goToTechnologyHtml();
 	}
 
+	function handleButtonBurgerMenuClick() {
+		openAsideMenu();
+		renderHtmlAsideMenu();
+	}
+
+	function handleButtonCloseAsideMenuClick() {
+		closeAsideMenu();
+		renderHtmlAsideMenu();
+	}
+
 	function goToHomeHtml() {
 		window.location.href = '../../html/home.html';
 	}
@@ -40,5 +56,22 @@ export default function Header() {
 	
 	function goToTechnologyHtml() {
 		window.location.href = '../../html/technology.html';
+	}
+
+	function openAsideMenu() {
+		burgerMenuVisible = !burgerMenuVisible;
+	}
+
+	function closeAsideMenu() {
+		burgerMenuVisible = false;
+	}
+
+	function renderHtmlAsideMenu() {
+		if (burgerMenuVisible) {
+			asideMenu.classList.add('header__aside--active')
+		} else {
+			asideMenu.classList.remove('header__aside--active');
+			burgerMenuVisible = false;
+		}
 	}
 }
