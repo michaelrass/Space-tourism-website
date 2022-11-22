8395bc0f1496a95c1afa2b9d7037302b5ff45363
your_username <michaelras_@live.com>
2022-11-22 10:10:30 +0100

    renaming html files and started error handling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 import Header from './modules/Header.js';
-import HomeButtonExplore from './modules/HomeButtonExplore.js';
 import Destination from './modules/Destination.js';
 import Crew from './modules/Crew.js';
 import Technology from './modules/Technology.js';
 
 Header();
-HomeButtonExplore();
 Destination();
 Crew();
 Technology();
