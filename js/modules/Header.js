69ce2e17c05f8e732386db0bf714e28d40706a26
your_username <michaelras_@live.com>
2022-11-22 09:40:26 +0100

    header.js clean up, going for a-href on html change

--- js/modules/Header.js
+++ js/modules/Header.js
@@ -1,47 +1,12 @@
-export default function Header() {
-	let burgerMenuVisible = false;
-	
-	const buttonHome = document.querySelector('.navigation__button-home');
-	const buttonDestination = document.querySelector('.navigation__button-destination');
-	const buttonCrew = document.querySelector('.navigation__button-crew');
-	const buttonTechnology = document.querySelector('.navigation__button-technology');
+export default function Header() {	
 	const buttonBurgerMenu = document.querySelector('.header__burger-icon-container');
 	const asideMenu = document.querySelector('.header__aside--hidden');
 	const buttonCloseAsideMenu = document.querySelector('.button-close-aside');
-	const burgerNavHome = document.querySelector('.top-container__button-in-aside-home');
-	const burgerNavDestination = document.querySelector('.top-container__button-in-aside-destination');
-	const burgerNavCrew = document.querySelector('.top-container__button-in-aside-crew');
-	const burgerNavTechnology = document.querySelector('.top-container__button-in-aside-technology');
-
-	buttonHome.addEventListener('click', handleButtonHomeClick);
-	buttonDestination.addEventListener('click', handleButtonDestinationClick);
-	buttonCrew.addEventListener('click', handleButtonCrewClick);
-	buttonTechnology.addEventListener('click', handleButtonTechnologyClick);
+	
+	let burgerMenuVisible = false;
+	
 	buttonBurgerMenu.addEventListener('click', handleButtonBurgerMenuClick);
 	buttonCloseAsideMenu.addEventListener('click', handleButtonCloseAsideMenuClick);
-
-	if (asideMenu) {
-		burgerNavHome.addEventListener('click', handleBurgerNavHomeClick);
-		burgerNavDestination.addEventListener('click', handleBurgerNavDestinationClick);
-		burgerNavCrew.addEventListener('click', handleBurgerNavCrewClick);
-		burgerNavTechnology.addEventListener('click', handleBurgerNavTechnologyClick);
-	}
-	
-	function handleButtonHomeClick() {
-		goToHomeHtml();
-	}
-
-	function handleButtonDestinationClick() {
-		goToDestinationHtml();
-	}
-
-	function handleButtonCrewClick() {
-		goToCrewHtml();
-	}
-
-	function handleButtonTechnologyClick() {
-		goToTechnologyHtml();
-	}
 
 	function handleButtonBurgerMenuClick() {
 		openAsideMenu();
@@ -51,38 +16,6 @@ export default function Header() {
 	function handleButtonCloseAsideMenuClick() {
 		closeAsideMenu();
 		renderHtmlAsideMenu();
-	}
-
-	function handleBurgerNavHomeClick() {
-		goToHomeHtml();
-	}
-
-	function handleBurgerNavDestinationClick() {
-		goToDestinationHtml();
-	}
-
-	function handleBurgerNavCrewClick() {
-		goToCrewHtml();
-	}
-
-	function handleBurgerNavTechnologyClick() {
-		goToTechnologyHtml();
-	}
-
-	function goToHomeHtml() {
-		window.location.href = '../../home.html';
-	}
-	
-	function goToDestinationHtml() {
-		window.location.href = '../../destination.html';
-	}
-	
-	function goToCrewHtml() {
-		window.location.href = '../../crew.html';
-	}
-	
-	function goToTechnologyHtml() {
-		window.location.href = '../../technology.html';
 	}
 
 	function openAsideMenu() {
